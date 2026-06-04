382de47b75490d0998ec82b6fc7e8b1d47d77daf
klarabelulaj <klara.belulaj@fshnstudent.info>
2026-06-04 12:04:32 +0200

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const obs = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((e) => {
+            if (e.isIntersecting) e.target.classList.add("visible");
+          });
+        },
+        { threshold: 0.1, rootMargin: "0px 0px -50px 0px" }
+      );
+      document.querySelectorAll(".reveal").forEach((el) => obs.observe(el));
